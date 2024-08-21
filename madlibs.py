295ea93a9a1e8de810528eaff87e93877cef8fe8
SANT0LA <103004629+SANT0LA@users.noreply.github.com>
2024-08-21 14:16:28 -0300

    Create madlibs.py
    Projeto numero 1 finalizado, pois fiz tudo de  uma vez

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+dia_da_semana = input("Dia da semana: ")
+sentimento = input("Digite um sentimento: ")
+data_especial = input("Digite uma data especial: ")
+verbo1 = input("Digite um verbo: ")
+verbo2 = input("Digite mais um verbo: ")
+adj_diminutivo = input("Digite um adjetivo no diminutivo: ")
+local = input("Digite um local:")
+verbo3 = input("Digite um verbo: ")
+substantivo = input("Digite um substantivo comum: ") 
+
+madlibs = (f"Hoje é {dia_da_semana} e eu estou muito {sentimento}.\nNa verdade eu gostaria que fosse {data_especial} para que eu pudesse {verbo1} e {verbo2}.\nTenho um amigo famoso. O {adj_diminutivo} e nós sempre vamos ao {local}. Qundo estamos lá nós gostamos de {verbo3} no {substantivo}")
+
+print(madlibs)
